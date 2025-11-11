b8e21c30fea8746739469c0665fa2bd573604e78
Ester <ester.hb@hotmail.com>
2025-11-11 14:08:53 -0300

    Adição de .cors(Customizer.withDefaults()) no httpsecurity

--- pi-senac-4/backend/api/src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
+++ pi-senac-4/backend/api/src/main/java/com/studybuddy/studybuddy_api/StudybuddyApiApplication.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class StudybuddyApiApplication {
 
     @Value("${spring.datasource.url}")
